ae97d697f7c972c04a23885b803aa0b5d746bc04
beethao <jasasoft@yahoo.com>
2015-02-12 11:00:45 -0600

    test adding folder

---
+++ valentine/assets/js/main.js
@@ -0,0 +1,5 @@
+$(".homepage-mbox h2").fitText(2, { minFontSize: '18px', maxFontSize: '30px' });
+$(".homepage-mbox .script-font").fitText(2, { minFontSize: '14px', maxFontSize: '26px' });
+$(".homepage-mbox .bold-font").fitText(2, { minFontSize: '16px', maxFontSize: '30px' });
+$(".homepage-mbox .location").fitText(2, { minFontSize: '12px', maxFontSize: '18px' });
+$(".homepage-mbox .subhead").fitText(3, { minFontSize: '11px', maxFontSize: '16px' });
